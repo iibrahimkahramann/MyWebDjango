5e509c47a87a5ad1d08cdb31b219932701789742
İbrahim Kahraman <ikahraman1938@gmail.com>
2023-11-10 23:11:59 +0300

    detay sayfasında kod eksiği tamamlanacak , skill sayfası tamamlandı

--- web/urls.py
+++ web/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', homepage, name='homepage'),
-    path('projects-detail/<slug>/', projects_details, name='post_details'),
+    path('projects-detail/<slug>/', projects_details, name='projects_details'),
     path('contact/', Contact, name='contact')
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
